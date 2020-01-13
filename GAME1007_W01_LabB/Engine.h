d5c6e3e2f065e31ce962b454281b1270b34ab545
NegarSaeidi <NegarSaeidi@users.noreply.github.com>
2020-01-12 19:38:08 -0500

    Add project files.

---
+++ GAME1007_W01_LabB/Engine.h
@@ -0,0 +1,32 @@
+#pragma once
+#include <SDL.h>
+#include <SDL_image.h>
+#include <SDL_mixer.h>
+#include <SDL_ttf.h>
+class Engine
+{
+private:   //Properties
+	bool g_bRunning; // Loop control flag.
+	int g_iSpeed = 5; // Speed of box.
+	const Uint8* g_iKeystates; // Keyboard state container.
+	Uint32 g_start, g_end, g_delta, g_fps; // Fixed timestep variables.
+	SDL_Window* g_pWindow; // This represents the SDL window.
+	SDL_Renderer* g_pRenderer; // This represents the buffer to draw to.
+
+private:   //methods
+	bool init(const char* , int , int , int , int, int);        //you could define all these functions inline(copy the whole function including its body here)
+	void wake();
+	void sleep();
+	void handleEvents();
+	bool keyDown(SDL_Scancode c);
+	void update();
+	void render();
+	void clean();
+
+
+public:
+	Engine();
+	int run();
+	~Engine();
+};
+
---
+++ GAME1007_W01_LabB/Main.cpp
@@ -0,0 +1,12 @@
+#include "Engine.h"
+
+
+// Global engine variables.
+
+
+// Main function.
+int main(int argc, char* args[]) // Main MUST have these parameters for SDL.
+{
+	Engine e;
+	return e.run();
+}
